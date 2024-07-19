8eb3b9ae00bced23c1151d81c80e9f8c0b4bff19
meloncholic-san <66624054+meloncholic-san@users.noreply.github.com>
2024-07-19 13:09:26 +0000

    Deploying to gh-pages from @ meloncholic-san/goit-js-hw-09@367f76d87228c7ca3f0222ac7fbc39bf4ce9f280 🚀

---
+++ 01-color-switcher.047b36e0.js
@@ -0,0 +1,2 @@
+const t={startButton:document.querySelector("button[data-start]"),stopButton:document.querySelector("button[data-stop]")};t.startButton.addEventListener("click",(function(){if(o)return;o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.startButton.disabled=!0,t.stopButton.disabled=!1})),t.stopButton.addEventListener("click",(function(){clearInterval(o),o=null,t.startButton.disabled=!1,t.stopButton.disabled=!0}));let o=null;
+//# sourceMappingURL=01-color-switcher.047b36e0.js.map
